af068c5d5121722dc69270a134716a3eb0146d83
UDDANTIAYYAPPA <uddantiayyappa42@gmail.com>
2022-11-11 19:28:37 +0530

    assaignments

--- Excel-Automation/src/com/sgtesting/excelassaignments/Assaignment6.java
+++ Excel-Automation/src/com/sgtesting/excelassaignments/Assaignment6.java
@@ -1,10 +1,50 @@
 package com.sgtesting.excelassaignments;
 
-public class Assaignment6 {
+import java.io.FileInputStream;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-	}
+public class Assaignment6
+{
 
+	public static void main(String[] args) 
+	{
+		try
+		{
+		String excelfilepath="E:\\EXCEL\\Countryname.xlsx";
+		FileInputStream fin=new FileInputStream(excelfilepath);
+		
+		
+		XSSFWorkbook workbook=new XSSFWorkbook(fin);
+		XSSFSheet sheet=workbook.getSheet("Sheet1");
+		
+		int rows=sheet.getLastRowNum();
+		int col=sheet.getRow(1).getLastCellNum();
+		for(int r=0;r<=rows;r++)
+		{
+		    XSSFRow row=sheet.getRow(r);
+		 
+		    for(int c=0;c<1;c++)
+		    {
+		      XSSFCell cell= row.getCell(c);
+		      switch(cell.getCellType())
+		      {
+		      case STRING:System.out.println(cell.getStringCellValue()); break;
+		     
+		      }
+		      System.out.print("     ");
+		    }
+		   System.out.println();    
+		}
+		  
+		
+	    }catch(Exception e)
+		{
+		e.printStackTrace();
+		}
 }
+}
+
